7ed4a32772645368ba51f527cd2c1e225d317a6e
Ingenieria <Ingenieria@MBQPAXIA051>
2024-06-01 16:56:08 -0500

    Intento 15001

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -40,7 +40,7 @@ public class Interfaz extends JFrame {
 	public Interfaz() {
 		setTitle("Galeria");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1700, 1000);
+		setBounds(100, 100, 1000, 500);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(253, 255, 252));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -50,7 +50,7 @@ public class Interfaz extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(30, 163, 177));
-		panel.setBounds(0, 0, 406, 961);
+		panel.setBounds(-30, -147, 406, 961);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -59,7 +59,7 @@ public class Interfaz extends JFrame {
 		panel.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(111, 329, 158, 66);
+		btnNewButton_1.setBounds(112, 162, 158, 66);
 		panel.add(btnNewButton_1);
 		
 		textField = new JTextField();
@@ -68,15 +68,15 @@ public class Interfaz extends JFrame {
 		textField.setColumns(10);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("New check box");
-		chckbxNewCheckBox.setBounds(75, 435, 97, 23);
+		chckbxNewCheckBox.setBounds(73, 301, 97, 23);
 		panel.add(chckbxNewCheckBox);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
-		rdbtnNewRadioButton.setBounds(99, 552, 109, 23);
+		rdbtnNewRadioButton.setBounds(179, 373, 109, 23);
 		panel.add(rdbtnNewRadioButton);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(433, 194, 233, 256);
+		scrollPane.setBounds(402, 61, 233, 256);
 		contentPane.add(scrollPane);
 		
 		JPanel panel_1 = new JPanel();
